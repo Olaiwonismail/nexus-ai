3aa17d3c5006c7b396ef29824302520e1df79972
Olaiwonismail <olaiwonismail@gmail.com>
2025-11-27 12:25:09 +0000

    blehh

---
+++ routes/health.py
@@ -0,0 +1,8 @@
+#rote for health check
+from flask import Blueprint, jsonify
+health_bp = Blueprint('health', __name__)
+@health_bp.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'healthy'}), 200
+
+    
